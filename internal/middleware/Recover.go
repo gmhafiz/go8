ca28917f03e175cca2059b1a8c99060908e4b5e0
Hafiz Shafruddin <hs@genisys.com.au>
2021-05-11 09:58:01 +1000

    add context to panic recoverer

--- internal/middleware/Recover.go
+++ internal/middleware/Recover.go
@@ -4,21 +4,38 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"runtime/debug"
 
 	chiMiddleware "github.com/go-chi/chi/middleware"
 )
 
+// Recovery adapted from https://github.com/go-chi/chi/blob/master/middleware/recoverer.go
 func Recovery(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("PANIC")
-		// notify to sentry/email/slack
-		log.Printf("request: %s %s\n", r.Method, r.URL.RequestURI())
-		body := r.GetBody
-		b, _ := json.Marshal(body)
-		log.Printf("host: %s\n", r.Host)
-		log.Printf("request body: %s\n", b)
-		chiMiddleware.Recoverer(next)
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if rvr := recover(); rvr != nil && rvr != http.ErrAbortHandler {
 
-		next.ServeHTTP(w, r)
-	})
+					logEntry := chiMiddleware.GetLogEntry(r)
+					if logEntry != nil {
+						logEntry.Panic(rvr, debug.Stack())
+					} else {
+						chiMiddleware.PrintPrettyStack(rvr)
+					}
+
+					log.Println("PANIC")
+					// notify to sentry/email/slack
+					log.Printf("request: %s %s\n", r.Method, r.URL.RequestURI())
+					body := r.GetBody
+					b, _ := json.Marshal(body)
+					log.Printf("host: %s\n", r.Host)
+					log.Printf("request body: %s\n", b)
+
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+			}()
+
+			next.ServeHTTP(w, r)
+		}
+
+	return http.HandlerFunc(fn)
 }

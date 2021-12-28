e5444e53455c825fb7000837e6c8508d554c0cbc
Hafiz Shafruddin <gmhafiz@gmail.com>
2021-12-29 10:54:29 +1100

    fix base filter with offset and limit

--- internal/utility/filter/base.go
+++ internal/utility/filter/base.go
@@ -10,31 +10,38 @@ const (
 	paginationDefaultPage = 1
 	paginationDefaultSize = 30
 
-	queryParamDisablePaging = "disable_paging"
 	queryParamPage          = "page"
-	queryParamSize          = "size"
+	queryParamLimit         = "limit"
+	queryParamOffset        = "offset"
+	queryParamDisablePaging = "disable_paging"
 	queryParamSort          = "sort"
 )
 
 type Filter struct {
-	Offset        int               `json:"page"`
-	Limit         uint              `json:"size"`
-	DisablePaging bool              `json:"disable_paging"`
-	Sort          map[string]string `json:"sort"`
-	Search        bool
+	Page          int  `json:"page"`
+	Offset        int  `json:"offset"`
+	Limit         uint `json:"size"`
+	DisablePaging bool `json:"disable_paging"`
+
+	Sort   map[string]string `json:"sort"`
+	Search bool
 }
 
 func New(queries url.Values) *Filter {
-	var page, size int
+	var page, limit, offset int
 	page, err := strconv.Atoi(queries.Get(queryParamPage))
 	if err != nil {
 		page = paginationDefaultPage
 	}
-	size, err = strconv.Atoi(queries.Get(queryParamSize))
+	limit, err = strconv.Atoi(queries.Get(queryParamLimit))
 	if err != nil {
-		size = paginationDefaultSize
+		limit = paginationDefaultSize
 	}
-	offset := size * (page - 1) // calculates offset
+
+	offset, err = strconv.Atoi(queries.Get(queryParamOffset))
+	if err != nil {
+		offset = limit * (page - 1) // calculates offset
+	}
 
 	disablePaging, _ := strconv.ParseBool(queries.Get(queryParamDisablePaging))
 
@@ -42,18 +49,26 @@ func New(queries url.Values) *Filter {
 	if queries.Has(queryParamSort) {
 		s := queries[queryParamSort]
 		for _, val := range s {
+			//// new strings.Cut() in go v1.18
+			//key, order, found := strings.Cut(val, ",")
+			//if found {
+			//	sortKey[key] = strings.ToUpper(order)
+			//} else {
+			//	sortKey[key] = "asc"
+			//}
 			split := strings.Split(val, ",")
 			if len(split) == 2 {
-				sortKey[split[0]] = split[1]
+				sortKey[split[0]] = strings.ToUpper(split[1])
 			} else {
-				sortKey[split[0]] = "asc"
+				sortKey[split[0]] = "ASC"
 			}
 		}
 	}
 
 	return &Filter{
+		Page:          page,
 		Offset:        offset,
-		Limit:         uint(size),
+		Limit:         uint(limit),
 		DisablePaging: disablePaging,
 		Sort:          sortKey,
 	}

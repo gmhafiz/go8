9a11738b134143eaf98b04ebd4e619b142832b05
Hafiz Shafruddin <gmhafiz@gmail.com>
2021-12-29 11:28:26 +1100

    cascade delete on pivot table

--- database/migrations/1632707632_book_authors_table.up.sql
+++ database/migrations/1632707632_book_authors_table.up.sql
@@ -1,8 +1,13 @@
 create table if not exists book_authors
 (
-    book_id bigserial not null,
-    author_id bigserial not null,
+    book_id bigserial not null
+        constraint book_authors_books_book_id_fk
+            references books
+            on delete cascade,
+    author_id bigserial not null
+        constraint book_authors_authors_id_fk
+            references authors
+            on delete cascade,
     constraint book_authors_pk
         primary key (book_id, author_id)
 );
-

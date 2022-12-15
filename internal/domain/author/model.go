f5431e7b314113618cb03d99bffac716021e5cd5
Hafiz Shafruddin <gmhafiz@gmail.com>
2022-12-15 20:34:38 +1100

    author has a schema model

---
+++ internal/domain/author/model.go
@@ -0,0 +1,18 @@
+package author
+
+import (
+	"time"
+
+	"github.com/gmhafiz/go8/internal/domain/book"
+)
+
+type Schema struct {
+	ID         uint
+	FirstName  string
+	MiddleName string
+	LastName   string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  *time.Time
+	Books      []*book.Schema
+}

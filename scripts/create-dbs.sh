003235df82297037c407510f600a8c29bbf0d9a0
Hafiz Shafruddin <gmhafiz@gmail.com>
2023-12-30 12:28:07 +1100

    fix test database creation for e2e test

--- scripts/create-dbs.sh
+++ scripts/create-dbs.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Copied from https://github.com/mrts/docker-postgresql-multiple-databases/blob/master/create-multiple-postgresql-databases.sh
+# Adapted from https://github.com/mrts/docker-postgresql-multiple-databases/blob/master/create-multiple-postgresql-databases.sh
 
 set -e
 set -u
 
-function create_user_and_database() {
+function create_database() {
 	local database=$1
-	echo "  Creating user and database '$database'"
+	echo "  Creating database '$database' for user '$POSTGRES_USER'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
 	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-		create_user_and_database $db
+		create_database $db
 	done
 	echo "Multiple databases created"
 fi
